390a9839713c1993ab3d71e8d848871ac3840d17
34-haziqdaraqutni <daraqutni92@gmail.com>
2021-10-12 15:05:45 +0800

    added the point system

--- Assets/playerscript.cs
+++ Assets/playerscript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class playerscript : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class playerscript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        score = 10;
+        score = 0;
         scoretext.GetComponent<Text>().text = "Score " + score;
     }
 
@@ -18,13 +19,21 @@ public class playerscript : MonoBehaviour
     void Update()
     {
         scoretext.GetComponent<Text>().text = "Score " + score;
+        if (score>=60)
+        {
+            SceneManager.LoadScene("winscene");
+        }
     }
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag.Equals("coin"))
         {
             score += 10;
             Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag.Equals("water"))
+        {
+            SceneManager.LoadScene("losescene");
         }
     }
 }
