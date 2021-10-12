ee88ab2d49f218e9d470b462d60668ab8ce3ed44
34-haziqdaraqutni <daraqutni92@gmail.com>
2021-10-12 14:57:21 +0800

    update the player script

--- Assets/playerscript.cs
+++ Assets/playerscript.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerscript : MonoBehaviour
 {
+    public GameObject scoretext;
+    private int score;
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = 10;
+        scoretext.GetComponent<Text>().text = "Score " + score;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoretext.GetComponent<Text>().text = "Score " + score;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("coin"))
+        {
+            score += 10;
+            Destroy(collision.gameObject);
+        }
     }
 }
+
